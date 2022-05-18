b9eb902876d6d9645fa657a861ff34f1858a9082
gutgara <olmiortizmoncayo@gmail.com>
2022-05-17 23:23:52 -0500

    no tocar iniciar estudiante

--- EducaPlay/app/src/main/java/com/paquete/educaplay/Iniciar_estudiante.java
+++ EducaPlay/app/src/main/java/com/paquete/educaplay/Iniciar_estudiante.java
@@ -1,22 +1,32 @@
 package com.paquete.educaplay;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
+
+import com.paquete.educaplay.ui.ConnectionClass;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Iniciar_estudiante extends AppCompatActivity {
     EditText usuario, contraseña;
     Button iniciar_sesion;
     Connection con;
+    String nombreusuario, nom;
     private Cursor fila;
 
     @Override
@@ -29,46 +39,84 @@ public class Iniciar_estudiante extends AppCompatActivity {
         iniciar_sesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                login();
-                Intent intent = new Intent(Iniciar_estudiante.this, MainActivity2.class);
-                startActivity(intent);
+                new checkLogin().execute("");
                 finish();
             }
         });
     }
 
-    public void login(){
-        Connection connection;
-        PreparedStatement ps;
-        try {
-            connection = DriverManager.getConnection("jdbc:jtds:sqlserver://gutgara.ddns.net;databaseName=EducaPlay;user=gutgara;password=VAuX2v_1xx0_T9w;");
-            ps = connection.prepareStatement("SELECT * FROM Usuarios WHERE Correo = '" + usuario.getText() + "' AND Pass = '" + contraseña.getText() + "' ");
-            ResultSet result = ps.executeQuery();
-            if(fila.moveToFirst()){
-                String usua=fila.getString(0);
-                String pass=fila.getString(1);
-                if (usuario.equals(usua)&&contraseña.equals(pass)){
-                    //Intent ven=new Intent(this, buscar_estudiante.class);
-                    //startActivity(ven);
+    public class checkLogin extends AsyncTask<String, String, String> {
+
+        String z = null;
+        Boolean isSuccess = false;
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            con = conexionBD(ConnectionClass.rol.toString(),ConnectionClass.nom.toString(), ConnectionClass.ape.toString(), ConnectionClass.correo.toString(), ConnectionClass.correo.toString(), ConnectionClass.contra.toString());
+            if (con == null) {
+                Toast.makeText(Iniciar_estudiante.this, "Revisa tu conexion", Toast.LENGTH_LONG).show();
+            } else {
+                try {
+
+                    String sql = "SELECT * FROM Usuarios WHERE Correo = '" + usuario.getText() + "' AND Pass = '" + contraseña.getText() + "' ";
+                    nombreusuario = usuario.getText().toString();
+                    nom = "" + nombreusuario;
+                    Statement stmt = con.createStatement();
+                    ResultSet rs = stmt.executeQuery(sql);
+                    if (rs.next()) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(Iniciar_estudiante.this, "Inicio de sesión exitoso", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                        z = "Success";
+                        Intent intent = new Intent(Iniciar_estudiante.this, MainActivity2.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(Iniciar_estudiante.this, "Revisa tu usuario o contraseña", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(Iniciar_estudiante.this, Iniciar_estudiante.class);
+                                startActivity(intent);
+                            }
+                        });
+                        usuario.setText("");
+                        contraseña.setText("");
+                    }
+
+                } catch (Exception e) {
+                    isSuccess = false;
+                    Log.e("SQL Error :", e.getMessage());
                 }
             }
-            else {
-                Toast toast=Toast.makeText(this,"Datos incorrectos",Toast.LENGTH_LONG);
-                toast.show();
-            }
-
-        }catch (Exception e) {
-            Toast toast=Toast.makeText(this,"Error" + e.getMessage(),Toast.LENGTH_LONG);
-            toast.show();
+            return z;
         }
     }
 
-    public void iniciobuscar(View view){
-        Intent bus = new Intent(this, MainActivity2.class);
-        startActivity(bus);
-    }
-    public void nuevoregistro(View view){
-        Intent in = new Intent(this,registrar_estudiante.class);
-        startActivity(in);
+    @SuppressLint("NewApi")
+    public Connection conexionBD(String rol,String nom, String ape, String corr, String usu, String cotra) {
+        Connection conexion = null;
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            conexion = DriverManager.getConnection("jdbc:jtds:sqlserver://gutgara.ddns.net;databaseName=EducaPlay;user=gutgara;password=VAuX2v_1xx0_T9w");
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        return conexion;
     }
 }
---
+++ EducaPlay/app/src/main/java/com/paquete/educaplay/ui/ConnectionClass.java
@@ -0,0 +1,11 @@
+package com.paquete.educaplay.ui;
+
+public class ConnectionClass {
+    public  static  String rol = "";
+    public  static  String nom = "";
+    public  static  String ape = "";
+    public  static  String correo = "";
+    public  static  String contra = "";
+
+
+}
